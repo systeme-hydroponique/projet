6e3797ea5925f1c2dbb39e7cf987179a88d64034
julies01 <julie.bened@utbm.fr>
2024-05-28 09:08:07 +0200

    debut fichier

---
+++ pageaccueil.php
@@ -0,0 +1,20 @@
+<DOCTYPE! html>
+<html lang="fr">
+<head>    
+	<meta charset = 'UTF-8' /> 
+	<meta name = 'viewport' content = 'width=device-width' initial-scale='1.0'/>
+	<title>Profil Administateur</title>
+	<!-->link rel="stylesheet" href="style_admin.css"</-->
+</head>
+<body>
+    <div id = 'contenant_1'>
+	<div id = 'conn'><a href = connexion.php>Se connecter </a></div>
+	<div id = 'insc'><a href = inscription.php>S'inscrire</a></div>
+	</div>
+
+	<div class = 'contenant'>
+	<h1>Bienvenue sur le site des clubs d'athlétisme de Belfort, Montbéliard et Sevenans </h1>
+	<div id = 'citation'> Citius, Altius, Fortius</div>
+	<img src="stade.jpeg" alt="Un stade d'athlétisme">
+	</div> 
+</body>
