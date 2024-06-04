12c36ce1686daa677c5438b3f53015b19a50b06a
julies01 <julie.bened@utbm.fr>
2024-06-04 16:07:17 +0200

    code de Mathéo et dernière modif base de données

--- dhydroponique.sql
+++ dhydroponique.sql
@@ -21,41 +21,48 @@ CREATE TABLE IF NOT EXISTS espece (
 
 CREATE TABLE IF NOT EXISTS plante (
     ID_plante INT(11) PRIMARY KEY AUTO_INCREMENT,
-    FOREIGN KEY (ID_espece) REFERENCES espece(ID_espece),
-    date_semis DATE,
+    ID_espece INT(11),
+    FOREIGN KEY(ID_espece) REFERENCES espece(ID_espece),
+    date_semis DATE
 );
 
 
 
-CREATE TABLE IF NOT EXISTS possession(
+CREATE TABLE IF NOT EXISTS possession (
+    ID_plante INT(11),
     FOREIGN KEY (ID_plante) REFERENCES plante(ID_plante),
-    FOREIGN KEY (ID_hdpc) REFERENCES hdpc(ID_hdpc),
+    ID_hdpc INT(11),
+    FOREIGN KEY (ID_hdpc) REFERENCES hdpc(ID_hdpc)
 );
 
 CREATE TABLE IF NOT EXISTS c_ensoleillement (
+    ID_plante INT(11),
     FOREIGN KEY (ID_plante) REFERENCES plante(ID_plante),
     lux INT(2),
     date_recuperation DATETIME 
 );
 
 CREATE TABLE IF NOT EXISTS c_humidite_sol (
+    ID_plante INT(11),
     FOREIGN KEY (ID_plante) REFERENCES plante(ID_plante),
     HR FLOAT(5),
     date_recuperation DATETIME 
 );
 
 CREATE TABLE IF NOT EXISTS c_pH (
+    ID_plante INT(11),
     FOREIGN KEY (ID_plante) REFERENCES plante(ID_plante),
     pH INT(1),
     date_recuperation DATETIME 
 );
 
-CREATE TABLE IF NOT EXISTS c_temperature (
+CREATE TABLE IF NOT EXISTS c_ (
     FOREIGN KEY (ID_plante) REFERENCES plante(ID_plante),
-    temperature INT(1),
+    pH INT(1),
     date_recuperation DATETIME 
 );
 CREATE TABLE IF NOT EXISTS c_niveau_eau (
+    ID_plante INT(11),
     FOREIGN KEY (ID_plante) REFERENCES plante(ID_plante),
     niveau INT(1),
     date_recuperation DATETIME 
