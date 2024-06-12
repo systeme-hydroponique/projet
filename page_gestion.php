5db5dfdfefa14f536144d05d7c26c89c981e03a3
irisbleues <manonbeauchamp@orange.fr>
2024-06-12 15:25:35 +0200

    modif css

--- page_gestion.php
+++ page_gestion.php
@@ -4,7 +4,7 @@
 	<meta charset = 'UTF-8' /> 
 	<meta name = 'viewport' content = 'width=device-width' initial-scale='1.0'/>
 	<title>Page gestion</title>
-	<!--<link rel="stylesheet" href="style_admin.css">-->
+	<link rel="stylesheet" href="graphisme.css">-->
 </head>
 <body>
 
--- page_inscription.php
+++ page_inscription.php
@@ -3,7 +3,7 @@
 	<head>    
 		<meta charset = 'UTF-8' /> 
 		<meta name = 'viewport' content = 'width=device-width; initial-scale=1.0'/>
-		<!--<link rel = 'stylesheet' href = 'style_conn_ins.css'/>-->
+		<link rel = 'stylesheet' href = 'graphisme.css'/>
 		<title>Inscription</title>
 	</head>
 	<body>
