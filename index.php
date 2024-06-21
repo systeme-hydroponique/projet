b432aeb64d50ae62a871a0d425f7fe75ddd79be8
Superpipo <75252692+noafouich@users.noreply.github.com>
2024-06-21 02:19:13 +0200

    Ajout responsive design
    Les écrans de téléphone étaient trop petits pour l'image de l'accueil,
    j'ai donc séparé les petits, moyens et grands appareils :
    - grand et moyen : 70% de la taille de l'image d'origine
    - petit (téléphone) : 50% de la taille de l'image d'origine

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>    
+    <meta charset="UTF-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="testcode.css">
+</head>
+<body>
+    <header class="sticky-header">
+        <nav>
+            <ul class="nav-list">
+                <li><a href="page_accueil.php">Accueil</a></li>
+                <li><a href="page_connexion.php">Se connecter</a></li>
+                <li><a href="page_inscription.php">S'inscrire</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="content">
+        <div id="contenant_1">
+            <!-- Contenu du contenant 1 -->
+        </div>
+        <div class="contenant">
+			<div id="titre_jardin">
+	    <h1 id="titre_1">Mon Jardin Hydroponique</h1></div>
+	    <div class="image-container">
+                <img class="responsive-img" src="icone_hydroponique.jpg" alt="Un plant hydroponique">
+	    </div>
+	</div>
+    </div>
+</body>
+</html>
